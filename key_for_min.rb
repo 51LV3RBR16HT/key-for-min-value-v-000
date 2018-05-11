5f0a9df575a4be6bf66695fa1e685d27d4019c2a
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-11 19:05:22 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,7 @@ def key_for_min_value(name_hash)
     if lowest_value == nil || value < lowest_value
       lowest_value = value
       lowest_key = key
-
   end
-
-  key_for_min_value
 end
+
+key_for_min_value
